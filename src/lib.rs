024940fff5d9b905a756958b44798981ad9abda1
Tinco Andringa <mail@tinco.nl>
2018-09-13 16:31:20 +0200

    introduce gc and memory heap structs and atomic reads

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,153 @@
 use futures::sync::oneshot;
 use futures_future::futures_future;
 use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+// Main malloc heap.
+// The heap itself is the "free[]" and "large" arrays,
+// but all the other global data is here too.
+//
+// mheap must not be heap-allocated because it contains mSpanLists,
+// which must not be heap-allocated.
+//
+//go:notinheap
+pub struct MemoryHeap {
+	// lock      mutex
+	// free      [_MaxMHeapList]mSpanList // free lists of given length up to _MaxMHeapList
+	// freelarge mTreap                   // free treap of length >= _MaxMHeapList
+	// busy      [_MaxMHeapList]mSpanList // busy lists of large spans of given length
+	// busylarge mSpanList                // busy lists of large spans length >= _MaxMHeapList
+	// sweepgen  uint32                   // sweep generation, see comment in mspan
+	sweep_done: AtomicUsize                   // all spans are swept
+	// sweepers  uint32                   // number of active sweepone calls
+    //
+	// // allspans is a slice of all mspans ever created. Each mspan
+	// // appears exactly once.
+	// //
+	// // The memory for allspans is manually managed and can be
+	// // reallocated and move as the heap grows.
+	// //
+	// // In general, allspans is protected by mheap_.lock, which
+	// // prevents concurrent access as well as freeing the backing
+	// // store. Accesses during STW might not hold the lock, but
+	// // must ensure that allocation cannot happen around the
+	// // access (since that may free the backing store).
+	// allspans []*mspan // all spans out there
+    //
+	// // spans is a lookup table to map virtual address page IDs to *mspan.
+	// // For allocated spans, their pages map to the span itself.
+	// // For free spans, only the lowest and highest pages map to the span itself.
+	// // Internal pages map to an arbitrary span.
+	// // For pages that have never been allocated, spans entries are nil.
+	// //
+	// // This is backed by a reserved region of the address space so
+	// // it can grow without moving. The memory up to len(spans) is
+	// // mapped. cap(spans) indicates the total reserved memory.
+	// spans []*mspan
+    //
+	// // sweepSpans contains two mspan stacks: one of swept in-use
+	// // spans, and one of unswept in-use spans. These two trade
+	// // roles on each GC cycle. Since the sweepgen increases by 2
+	// // on each cycle, this means the swept spans are in
+	// // sweepSpans[sweepgen/2%2] and the unswept spans are in
+	// // sweepSpans[1-sweepgen/2%2]. Sweeping pops spans from the
+	// // unswept stack and pushes spans that are still in-use on the
+	// // swept stack. Likewise, allocating an in-use span pushes it
+	// // on the swept stack.
+	// sweepSpans [2]gcSweepBuf
+    //
+	// _ uint32 // align uint64 fields on 32-bit for atomics
+    //
+	// // Proportional sweep
+	// //
+	// // These parameters represent a linear function from heap_live
+	// // to page sweep count. The proportional sweep system works to
+	// // stay in the black by keeping the current page sweep count
+	// // above this line at the current heap_live.
+	// //
+	// // The line has slope sweepPagesPerByte and passes through a
+	// // basis point at (sweepHeapLiveBasis, pagesSweptBasis). At
+	// // any given time, the system is at (memstats.heap_live,
+	// // pagesSwept) in this space.
+	// //
+	// // It's important that the line pass through a point we
+	// // control rather than simply starting at a (0,0) origin
+	// // because that lets us adjust sweep pacing at any time while
+	// // accounting for current progress. If we could only adjust
+	// // the slope, it would create a discontinuity in debt if any
+	// // progress has already been made.
+	// pagesInUse         uint64  // pages of spans in stats _MSpanInUse; R/W with mheap.lock
+	// pagesSwept         uint64  // pages swept this cycle; updated atomically
+	// pagesSweptBasis    uint64  // pagesSwept to use as the origin of the sweep ratio; updated atomically
+	// sweepHeapLiveBasis uint64  // value of heap_live to use as the origin of sweep ratio; written with lock, read without
+	// sweepPagesPerByte  float64 // proportional sweep ratio; written with lock, read without
+	// // TODO(austin): pagesInUse should be a uintptr, but the 386
+	// // compiler can't 8-byte align fields.
+    //
+	// // Malloc stats.
+	// largealloc  uint64                  // bytes allocated for large objects
+	// nlargealloc uint64                  // number of large object allocations
+	// largefree   uint64                  // bytes freed for large objects (>maxsmallsize)
+	// nlargefree  uint64                  // number of frees for large objects (>maxsmallsize)
+	// nsmallfree  [_NumSizeClasses]uint64 // number of frees for small objects (<=maxsmallsize)
+    //
+	// // range of addresses we might see in the heap
+	// bitmap        uintptr // Points to one byte past the end of the bitmap
+	// bitmap_mapped uintptr
+    //
+	// // The arena_* fields indicate the addresses of the Go heap.
+	// //
+	// // The maximum range of the Go heap is
+	// // [arena_start, arena_start+_MaxMem+1).
+	// //
+	// // The range of the current Go heap is
+	// // [arena_start, arena_used). Parts of this range may not be
+	// // mapped, but the metadata structures are always mapped for
+	// // the full range.
+	// arena_start uintptr
+	// arena_used  uintptr // Set with setArenaUsed.
+    //
+	// // The heap is grown using a linear allocator that allocates
+	// // from the block [arena_alloc, arena_end). arena_alloc is
+	// // often, but *not always* equal to arena_used.
+	// arena_alloc uintptr
+	// arena_end   uintptr
+    //
+	// // arena_reserved indicates that the memory [arena_alloc,
+	// // arena_end) is reserved (e.g., mapped PROT_NONE). If this is
+	// // false, we have to be careful not to clobber existing
+	// // mappings here. If this is true, then we own the mapping
+	// // here and *must* clobber it to use it.
+	// arena_reserved bool
+    //
+	// _ uint32 // ensure 64-bit alignment
+    //
+	// // central free lists for small size classes.
+	// // the padding makes sure that the MCentrals are
+	// // spaced CacheLineSize bytes apart, so that each MCentral.lock
+	// // gets its own cache line.
+	// // central is indexed by spanClass.
+	// central [numSpanClasses]struct {
+	// 	mcentral mcentral
+	// 	pad      [sys.CacheLineSize - unsafe.Sizeof(mcentral{})%sys.CacheLineSize]byte
+	// }
+    //
+	// spanalloc             fixalloc // allocator for span*
+	// cachealloc            fixalloc // allocator for mcache*
+	// treapalloc            fixalloc // allocator for treapNodes* used by large objects
+	// specialfinalizeralloc fixalloc // allocator for specialfinalizer*
+	// specialprofilealloc   fixalloc // allocator for specialprofile*
+	// speciallock           mutex    // lock for special record allocators.
+}
+
+pub fn new_memory_heap() -> MemoryHeap {
+    MemoryHeap {
+        sweep_done: AtomicUsize::new(0),
+    }
+}
 
 // State of background sweep.
-pub struct SweepData {
+pub struct BackgroundSweep {
 	parked:  bool,
 	started: bool,
 
@@ -26,7 +170,7 @@ pub struct SweepData {
 	sweep_pauses:  u32,
 }
 
-impl SweepData {
+impl BackgroundSweep {
     fn park(&mut self) {
         self.parked = true;
     }
@@ -38,9 +182,8 @@ impl SweepData {
     }
 }
 
-pub fn new_sweep() -> SweepData {
-
-    SweepData {
+pub fn new_sweep() -> BackgroundSweep {
+    BackgroundSweep {
         parked: false,
         started: false,
         gc_ready_sender: None,
@@ -49,116 +192,132 @@ pub fn new_sweep() -> SweepData {
     }
 }
 
-type SweepReference = Arc<Mutex<SweepData>>;
+type SweepReference = Mutex<BackgroundSweep>;
 
-// gcenable is called after the bulk of the runtime initialization,
-// just before we're about to start letting user code run.
-// It kicks off the background sweeper goroutine and enables GC.
-pub async fn gc_enable() {
-    let (signal_setup_done, mut setup_done) = oneshot::channel::<bool>();
-
-    // TODO we should get sweep from a global GC object
-    let sweep = Arc::new(Mutex::new(new_sweep()));
-
-    background_sweep(sweep.clone(), signal_setup_done);
-
-    let f = futures_future(&mut setup_done);
-
-    await!(f);
+pub struct GC {
+    background_sweep: Mutex<BackgroundSweep>,
+    memory_heap: MemoryHeap,
 }
 
-async fn background_sweep(sweep: SweepReference, signal_setup_done: oneshot::Sender<bool>) {
-    let mut gc_ready = {
-        let mut sweep = sweep.lock().expect("Could not get lock on SweepData");
-        sweep.park();
-
-        let _ = signal_setup_done.send(true);
-
-        sweep.wait_for_gc_start()
-    };
-
-    await!(futures_future(&mut gc_ready));
-
-    // for { // forever do
-    // we sweep one and then yield to other goroutines until sweepone returns FFFFFFF
-	// 	for gosweepone() != ^uintptr(0) { // if gosweepone != complement of 0 (so FFF?)
-	// 		sweep.nbgsweep++
-	// 		Gosched()
-	// 	}
-    //
-    // If gosweepone returned FFFFF we freeSomeWbufs until that returns false, yielding in between
-	// 	for freeSomeWbufs(true) {	// 		Gosched()}
-    // Then we get the sweep lock
-	// 	lock(&sweep.lock)
-    //
-	// 	if !gosweepdone() {
-	// 		// This can happen if a GC runs between
-	// 		// gosweepone returning ^0 above
-	// 		// and the lock being acquired.
-	// 		unlock(&sweep.lock)
-	// 		continue
-	// 	}
-	// 	sweep.parked = true
-	// 	goparkunlock(&sweep.lock, waitReasonGCSweepWait, traceEvGoBlock, 1)
-	// }
+pub fn new_gc() -> GC {
+    GC {
+        background_sweep: Mutex::new(new_sweep()),
+        memory_heap: new_memory_heap(),
+    }
 }
 
-// sweeps one span
-// returns number of pages returned to heap, or ^uintptr(0) if there is nothing to sweep
-//go:nowritebarrier
-pub fn sweep_one() /* -> uintptr */ {
-	// _g_ := getg()
-	// sweepRatio := mheap_.sweepPagesPerByte // For debugging
-    //
-	// // increment locks to ensure that the goroutine is not preempted
-	// // in the middle of sweep thus leaving the span in an inconsistent state for next GC
-	// _g_.m.locks++
-	// if atomic.Load(&mheap_.sweepdone) != 0 {
-	// 	_g_.m.locks--
-	// 	return ^uintptr(0)
-	// }
-	// atomic.Xadd(&mheap_.sweepers, +1)
-    //
-	// npages := ^uintptr(0)
-	// sg := mheap_.sweepgen
-	// for {
-	// 	s := mheap_.sweepSpans[1-sg/2%2].pop()
-	// 	if s == nil {
-	// 		atomic.Store(&mheap_.sweepdone, 1)
-	// 		break
-	// 	}
-	// 	if s.state != mSpanInUse {
-	// 		// This can happen if direct sweeping already
-	// 		// swept this span, but in that case the sweep
-	// 		// generation should always be up-to-date.
-	// 		if s.sweepgen != sg {
-	// 			print("runtime: bad span s.state=", s.state, " s.sweepgen=", s.sweepgen, " sweepgen=", sg, "\n")
-	// 			throw("non in-use span in unswept list")
-	// 		}
-	// 		continue
-	// 	}
-	// 	if s.sweepgen != sg-2 || !atomic.Cas(&s.sweepgen, sg-2, sg-1) {
-	// 		continue
-	// 	}
-	// 	npages = s.npages
-	// 	if !s.sweep(false) {
-	// 		// Span is still in-use, so this returned no
-	// 		// pages to the heap and the span needs to
-	// 		// move to the swept in-use list.
-	// 		npages = 0
-	// 	}
-	// 	break
-	// }
-    //
-	// // Decrement the number of active sweepers and if this is the
-	// // last one print trace information.
-	// if atomic.Xadd(&mheap_.sweepers, -1) == 0 && atomic.Load(&mheap_.sweepdone) != 0 {
-	// 	if debug.gcpacertrace > 0 {
-	// 		print("pacer: sweep done at heap size ", memstats.heap_live>>20, "MB; allocated ", (memstats.heap_live-mheap_.sweepHeapLiveBasis)>>20, "MB during sweep; swept ", mheap_.pagesSwept, " pages at ", sweepRatio, " pages/byte\n")
-	// 	}
-	// }
-	// _g_.m.locks--
-	// return npages
+impl GC {
+    // gcenable is called after the bulk of the runtime initialization,
+    // just before we're about to start letting user code run.
+    // It kicks off the background sweeper routine and enables GC.
+    pub async fn enable(&mut self) {
+        let (signal_setup_done, mut setup_done) = oneshot::channel::<bool>();
+
+        self.background_sweep(signal_setup_done);
+
+        let f = futures_future(&mut setup_done);
+
+        await!(f);
+    }
+
+    async fn background_sweep(&mut self, signal_setup_done: oneshot::Sender<bool>) {
+        // This assignment is to a block, so that the lock is released at the end of it and
+        // we can safely wait for something to signal us that the gc is ready.
+        let mut gc_ready = {
+            let mut sweep = self.background_sweep.lock().expect("Could not get lock on SweepData");
+            sweep.park();
+
+            let _ = signal_setup_done.send(true);
+
+            sweep.wait_for_gc_start()
+        };
+
+        await!(futures_future(&mut gc_ready));
+
+        loop { // forever
+        // we sweep one and then yield to other goroutines until sweepone returns FFFFFFF
+    	// for gosweepone() != ^uintptr(0) { // if gosweepone != complement of 0 (so FFF?)
+    	// 		sweep.nbgsweep++
+    	// 		Gosched()
+    	// 	}
+        //
+        // If gosweepone returned FFFFF we freeSomeWbufs until that returns false, yielding in between
+    	// 	for freeSomeWbufs(true) {	// 		Gosched()}
+        // Then we get the sweep lock
+    	// 	lock(&sweep.lock)
+        //
+    	// 	if !gosweepdone() {
+    	// 		// This can happen if a GC runs between
+    	// 		// gosweepone returning ^0 above
+    	// 		// and the lock being acquired.
+    	// 		unlock(&sweep.lock)
+    	// 		continue
+    	// 	}
+    	// 	sweep.parked = true
+    	// 	goparkunlock(&sweep.lock, waitReasonGCSweepWait, traceEvGoBlock, 1)
+    	}
+    }
+
+    // sweeps one span
+    // returns number of pages returned to heap, or !0 if there is nothing to sweep
+    //go:nowritebarrier
+    pub fn sweep_one(&mut self) -> usize {
+    	// sweepRatio := mheap_.sweepPagesPerByte // For debugging
+
+        // Go has preemption at every function invocation, so even during garbage collection
+        // other goroutines might be preempt the current thread. To prevent that Go increases
+        // the locks field on the native thread. Since in Rust we can't be preempted inside a
+        // thread we don't have to worry about that.
+
+        // TODO: I think go:nowritebarrier means the ordering here can be Relaxed, is that correct?
+        if self.memory_heap.sweep_done.load(Ordering::Relaxed) != 0 {
+    	 	!0
+    	} else {
+            0
+        }
+    	// atomic.Xadd(&mheap_.sweepers, +1)
+        //
+    	// npages := ^uintptr(0)
+    	// sg := mheap_.sweepgen
+    	// for {
+    	// 	s := mheap_.sweepSpans[1-sg/2%2].pop()
+    	// 	if s == nil {
+    	// 		atomic.Store(&mheap_.sweepdone, 1)
+    	// 		break
+    	// 	}
+    	// 	if s.state != mSpanInUse {
+    	// 		// This can happen if direct sweeping already
+    	// 		// swept this span, but in that case the sweep
+    	// 		// generation should always be up-to-date.
+    	// 		if s.sweepgen != sg {
+    	// 			print("runtime: bad span s.state=", s.state, " s.sweepgen=", s.sweepgen, " sweepgen=", sg, "\n")
+    	// 			throw("non in-use span in unswept list")
+    	// 		}
+    	// 		continue
+    	// 	}
+    	// 	if s.sweepgen != sg-2 || !atomic.Cas(&s.sweepgen, sg-2, sg-1) {
+    	// 		continue
+    	// 	}
+    	// 	npages = s.npages
+    	// 	if !s.sweep(false) {
+    	// 		// Span is still in-use, so this returned no
+    	// 		// pages to the heap and the span needs to
+    	// 		// move to the swept in-use list.
+    	// 		npages = 0
+    	// 	}
+    	// 	break
+    	// }
+        //
+    	// // Decrement the number of active sweepers and if this is the
+    	// // last one print trace information.
+    	// if atomic.Xadd(&mheap_.sweepers, -1) == 0 && atomic.Load(&mheap_.sweepdone) != 0 {
+    	// 	if debug.gcpacertrace > 0 {
+    	// 		print("pacer: sweep done at heap size ", memstats.heap_live>>20, "MB; allocated ", (memstats.heap_live-mheap_.sweepHeapLiveBasis)>>20, "MB during sweep; swept ", mheap_.pagesSwept, " pages at ", sweepRatio, " pages/byte\n")
+    	// 	}
+    	// }
+    	// _g_.m.locks--
+    	// return npages
+    }
 }
 
 // Sweep frees or collects finalizers for blocks not marked in the mark phase.
