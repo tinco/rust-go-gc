408ab672d5fe80b870d83f4befcd8f44261c51c7
Tinco Andringa <mail@tinco.nl>
2018-09-20 01:48:47 +0200

    figure out how to alloc

--- src/lib.rs
+++ src/lib.rs
@@ -9,182 +9,20 @@
 #![feature(pin)]
 #![feature(arbitrary_self_types)]
 #![feature(generators)]
+#![feature(allocator_api)]
 
+
+mod memory_span;
 mod sweep_buffer;
+mod memory_heap;
 
 use futures::sync::{oneshot, mpsc};
 use futures::stream::Stream;
 use futures_future::futures_future;
-use std::sync::{Arc, Mutex};
+use std::sync::{Mutex};
 use std::sync::atomic::{AtomicUsize, AtomicBool, Ordering};
 
-
-// Main malloc heap.
-// The heap itself is the "free[]" and "large" arrays,
-// but all the other global data is here too.
-//
-// mheap must not be heap-allocated because it contains mSpanLists,
-// which must not be heap-allocated.
-//
-//go:notinheap
-pub struct MemoryHeap {
-    // lock      mutex
-    // free      [_MaxMHeapList]mSpanList // free lists of given length up to _MaxMHeapList
-    // freelarge mTreap                   // free treap of length >= _MaxMHeapList
-    // busy      [_MaxMHeapList]mSpanList // busy lists of large spans of given length
-    // busylarge mSpanList                // busy lists of large spans length >= _MaxMHeapList
-    // TODO these should all be u32 for locality optimization?
-    sweep_generation:  AtomicUsize,       // sweep generation, see comment in mspan, is increased by 2 after every GC
-    sweep_done: AtomicBool,              // all spans are swept
-    sweepers: AtomicUsize,                 // number of active sweepone calls
-    //
-    // // allspans is a slice of all mspans ever created. Each mspan
-    // // appears exactly once.
-    // //
-    // // The memory for allspans is manually managed and can be
-    // // reallocated and move as the heap grows.
-    // //
-    // // In general, allspans is protected by mheap_.lock, which
-    // // prevents concurrent access as well as freeing the backing
-    // // store. Accesses during STW might not hold the lock, but
-    // // must ensure that allocation cannot happen around the
-    // // access (since that may free the backing store).
-    // allspans []*mspan // all spans out there
-    //
-    // // spans is a lookup table to map virtual address page IDs to *mspan.
-    // // For allocated spans, their pages map to the span itself.
-    // // For free spans, only the lowest and highest pages map to the span itself.
-    // // Internal pages map to an arbitrary span.
-    // // For pages that have never been allocated, spans entries are nil.
-    // //
-    // // This is backed by a reserved region of the address space so
-    // // it can grow without moving. The memory up to len(spans) is
-    // // mapped. cap(spans) indicates the total reserved memory.
-    // spans []*mspan
-    //
-
-    // sweep_buffers contains two mspan stacks: one of swept in-use
-    // spans, and one of unswept in-use spans. These two trade
-    // roles on each GC cycle. Since the sweepgen increases by 2
-    // on each cycle, this means the swept spans are in
-    // sweep_buffers[sweepgen/2%2] and the unswept spans are in
-    // sweep_buffers[1-sweepgen/2%2]. Sweeping pops spans from the
-    // unswept stack and pushes spans that are still in-use on the
-    // swept stack. Likewise, allocating an in-use span pushes it
-    // on the swept stack.
-    sweep_buffers: [sweep_buffer::SweepBuffer;2],
-
-    //
-    // _ uint32 // align uint64 fields on 32-bit for atomics
-    //
-    // // Proportional sweep
-    // //
-    // // These parameters represent a linear function from heap_live
-    // // to page sweep count. The proportional sweep system works to
-    // // stay in the black by keeping the current page sweep count
-    // // above this line at the current heap_live.
-    // //
-    // // The line has slope sweepPagesPerByte and passes through a
-    // // basis point at (sweepHeapLiveBasis, pagesSweptBasis). At
-    // // any given time, the system is at (memstats.heap_live,
-    // // pagesSwept) in this space.
-    // //
-    // // It's important that the line pass through a point we
-    // // control rather than simply starting at a (0,0) origin
-    // // because that lets us adjust sweep pacing at any time while
-    // // accounting for current progress. If we could only adjust
-    // // the slope, it would create a discontinuity in debt if any
-    // // progress has already been made.
-    // pagesInUse         uint64  // pages of spans in stats _MSpanInUse; R/W with mheap.lock
-    // pagesSwept         uint64  // pages swept this cycle; updated atomically
-    // pagesSweptBasis    uint64  // pagesSwept to use as the origin of the sweep ratio; updated atomically
-    // sweepHeapLiveBasis uint64  // value of heap_live to use as the origin of sweep ratio; written with lock, read without
-    // sweepPagesPerByte  float64 // proportional sweep ratio; written with lock, read without
-    // // TODO(austin): pagesInUse should be a uintptr, but the 386
-    // // compiler can't 8-byte align fields.
-    //
-    // // Malloc stats.
-    // largealloc  uint64                  // bytes allocated for large objects
-    // nlargealloc uint64                  // number of large object allocations
-    // largefree   uint64                  // bytes freed for large objects (>maxsmallsize)
-    // nlargefree  uint64                  // number of frees for large objects (>maxsmallsize)
-    // nsmallfree  [_NumSizeClasses]uint64 // number of frees for small objects (<=maxsmallsize)
-    //
-    // // range of addresses we might see in the heap
-    // bitmap        uintptr // Points to one byte past the end of the bitmap
-    // bitmap_mapped uintptr
-    //
-    // // The arena_* fields indicate the addresses of the Go heap.
-    // //
-    // // The maximum range of the Go heap is
-    // // [arena_start, arena_start+_MaxMem+1).
-    // //
-    // // The range of the current Go heap is
-    // // [arena_start, arena_used). Parts of this range may not be
-    // // mapped, but the metadata structures are always mapped for
-    // // the full range.
-    // arena_start uintptr
-    // arena_used  uintptr // Set with setArenaUsed.
-    //
-    // // The heap is grown using a linear allocator that allocates
-    // // from the block [arena_alloc, arena_end). arena_alloc is
-    // // often, but *not always* equal to arena_used.
-    // arena_alloc uintptr
-    // arena_end   uintptr
-    //
-    // // arena_reserved indicates that the memory [arena_alloc,
-    // // arena_end) is reserved (e.g., mapped PROT_NONE). If this is
-    // // false, we have to be careful not to clobber existing
-    // // mappings here. If this is true, then we own the mapping
-    // // here and *must* clobber it to use it.
-    // arena_reserved bool
-    //
-    // _ uint32 // ensure 64-bit alignment
-    //
-    // // central free lists for small size classes.
-    // // the padding makes sure that the MCentrals are
-    // // spaced CacheLineSize bytes apart, so that each MCentral.lock
-    // // gets its own cache line.
-    // // central is indexed by spanClass.
-    // central [numSpanClasses]struct {
-    // 	mcentral mcentral
-    // 	pad      [sys.CacheLineSize - unsafe.Sizeof(mcentral{})%sys.CacheLineSize]byte
-    // }
-    //
-    // spanalloc             fixalloc // allocator for span*
-    // cachealloc            fixalloc // allocator for mcache*
-    // treapalloc            fixalloc // allocator for treapNodes* used by large objects
-    // specialfinalizeralloc fixalloc // allocator for specialfinalizer*
-    // specialprofilealloc   fixalloc // allocator for specialprofile*
-    // speciallock           mutex    // lock for special record allocators.
-}
-
-impl MemoryHeap {
-    pub fn get_pushable_sweep_buffer(&self, generation: usize) -> &sweep_buffer::PushableSweepBuffer {
-        unsafe {
-            std::mem::transmute::<
-                &sweep_buffer::SweepBuffer,
-                &sweep_buffer::PushableSweepBuffer>(&self.sweep_buffers[generation/2%2])
-        }
-    }
-
-    pub fn get_popable_sweep_buffer(&self, generation: usize) -> &sweep_buffer::PopableSweepBuffer {
-        unsafe {
-            std::mem::transmute::<
-                &sweep_buffer::SweepBuffer,
-                &sweep_buffer::PopableSweepBuffer>(&self.sweep_buffers[1 - generation/2%2])
-        }
-    }
-}
-
-pub fn new_memory_heap() -> MemoryHeap {
-    MemoryHeap {
-        sweep_done: AtomicBool::new(false),
-        sweepers: AtomicUsize::new(0),
-        sweep_generation: AtomicUsize::new(0),
-        sweep_buffers: [sweep_buffer::SweepBuffer{}, sweep_buffer::SweepBuffer{}],
-    }
-}
+use self::memory_heap::*;
 
 // State of background sweep.
 pub struct BackgroundSweep {
@@ -334,7 +172,7 @@ impl GC {
             let sweep_generation = self.memory_heap.sweep_generation.load(Ordering::Relaxed);
 
             loop {
-                // let sweep = self.memory_heap.sweep_spans.get(1 - sweep_generation / 2 % 2).pop();
+                let sweep = self.memory_heap.get_popable_sweep_buffer(sweep_generation).pop();
                 // 	if s == nil {
                 // 		atomic.Store(&mheap_.sweepdone, 1)
                 // 		break
--- src/sweep_buffer.rs
+++ src/sweep_buffer.rs
@@ -1,11 +1,20 @@
+
+use super::memory_span::*;
+use std::sync::atomic::{AtomicUsize, AtomicPtr, Ordering};
+use std::sync::{Mutex};
+use std::alloc::*;
+
+const GC_SWEEP_BLOCK_ENTRIES: usize = 512; // 4KB on 64-bit
+const GC_SWEEP_BUF_INIT_SPINE_CAP: usize = 256; // Enough for 1GB heap on 64-bit
+
 pub struct SweepBuffer {
-    // A gcSweepBuf is a two-level data structure consisting of a
+    // A SweepBuffer is a two-level data structure consisting of a
 	// growable spine that points to fixed-sized blocks. The spine
 	// can be accessed without locks, but adding a block or
 	// growing it requires taking the spine lock.
 	//
-	// Because each mspan covers at least 8K of heap and takes at
-	// most 8 bytes in the gcSweepBuf, the growth of the spine is
+	// Because each MemorySpan covers at least 8K of heap and takes at
+	// most 8 bytes in the SweepBuffer, the growth of the spine is
 	// quite limited.
 	//
 	// The spine and all blocks are allocated off-heap, which
@@ -17,17 +26,129 @@ pub struct SweepBuffer {
 
 	// spineLock mutex
 	// spine     unsafe.Pointer // *[N]*gcSweepBlock, accessed atomically
-	// spineLen  uintptr        // Spine array length, accessed atomically
-	// spineCap  uintptr        // Spine array cap, accessed under lock
     //
 	// // index is the first unused slot in the logical concatenation
 	// // of all blocks. It is accessed atomically.
-	// index uint32
-
-    // All operations on SweepBuffer are unsafe, as we can not
-    // express the dual personalities of SweepBuffer in Rust's
-    // TypeSystem without resorting to Trait Objects.
+    pub index: AtomicUsize,
+    pub spine_length: AtomicUsize, // Spine array length, accessed atomically
+    pub spine_cap: Mutex<usize>, // Spine array cap, accessed under lock
+    pub spine: AtomicPtr<SweepBlock>, // TODO write persistentalloc for possibly more performance
 }
 
-pub struct PushableSweepBuffer(SweepBuffer);
-pub struct PopableSweepBuffer(SweepBuffer);
+pub struct SweepBlockData {
+	spans: [MemorySpan; GC_SWEEP_BLOCK_ENTRIES]
+}
+
+pub type SweepBlock = &'static mut SweepBlockData;
+
+impl SweepBuffer {
+    pub fn new() -> SweepBuffer {
+        SweepBuffer {
+            index: AtomicUsize::new(0),
+            spine_length: AtomicUsize::new(0),
+            spine_cap: Mutex::new(0),
+            spine: Default::default(),
+        }
+    }
+}
+
+pub struct PushableSweepBuffer(pub SweepBuffer);
+pub struct PopableSweepBuffer(pub SweepBuffer);
+
+impl PopableSweepBuffer {
+    // pop removes and returns a span from buffer b, or nil if b is empty.
+    // pop is safe to call concurrently with other pop operations, but NOT
+    // to call concurrently with push.
+    pub fn pop(&self) -> Option<MemorySpan> {
+    	let cursor = self.0.index.fetch_sub(1, Ordering::Relaxed);
+    	if cursor == 0 {
+            self.0.index.fetch_add(1, Ordering::Relaxed);
+            None
+    	} else {
+            let cursor = cursor - 1; // fetch_sub returns old cursor
+            // There are no concurrent spine or block modifications during
+            // pop, so we can omit the atomics.
+            // TODO Unfortunately it's unstable to read atomics unatomically in Rust do this later?
+            let (top, bottom) = (cursor / GC_SWEEP_BLOCK_ENTRIES, cursor % GC_SWEEP_BLOCK_ENTRIES);
+            // blockp := (**gcSweepBlock)(b.spine + sys.PtrSize*uintptr(top)))
+            // block := *blockp
+            // s := block.spans[bottom]
+            // // Clear the pointer for block(i).
+            // block.spans[bottom] = nil
+            // return s
+            None
+        }
+    }
+}
+
+impl PushableSweepBuffer {
+    // push adds span s to buffer b. push is safe to call concurrently
+    // with other push operations, but NOT to call concurrently with pop.
+    pub fn push(&self, span: MemorySpan) {
+        // Obtain our slot.
+        let cursor = self.0.index.fetch_add(1, Ordering::Relaxed);
+        let (top, bottom) = (cursor / GC_SWEEP_BLOCK_ENTRIES, cursor % GC_SWEEP_BLOCK_ENTRIES);
+
+    	// Do we need to add a block?
+        let mut spine_length = self.0.spine_length.load(Ordering::Relaxed);
+        let block : SweepBlock = loop {
+            if(top < spine_length) {
+                // 		spine := atomic.Loadp(unsafe.Pointer(&b.spine))
+                // 		blockp := add(spine, sys.PtrSize*top)
+                // 		block = (*gcSweepBlock)(atomic.Loadp(blockp))
+            } else {
+            	// Add a new block to the spine, potentially growing
+                // the spine.
+                let spine_cap = *self.0.spine_cap.lock().expect("Could not unlock spine cap");
+         		// spine_length cannot change until we release the lock,
+         		// but may have changed while we were waiting.
+                spine_length = self.0.spine_length.load(Ordering::Relaxed);
+        		if top < spine_length {
+        			continue
+        		} else {
+                    if spine_length == spine_cap {
+            			// Grow the spine.
+                        let new_cap = if spine_cap != 0 {
+                            spine_cap * 2
+                        } else {
+                        	GC_SWEEP_BUF_INIT_SPINE_CAP
+                        };
+
+                        // TODO newSpine := persistentalloc(newCap*sys.PtrSize, sys.CacheLineSize, &memstats.gc_sys)
+                        let mut new_spine = unsafe {
+                            alloc(Layout::array::<SweepBlock>(new_cap).expect("Could not layout spine"))
+                        };
+
+                        if spine_cap != 0 {
+            				// Blocks are allocated off-heap, so
+            				// no write barriers.
+            			    // memmove(newSpine, b.spine, b.spineCap*sys.PtrSize)
+            			}
+            			// // Spine is allocated off-heap, so no write barrier.
+            			// atomic.StorepNoWB(unsafe.Pointer(&b.spine), newSpine)
+            			// b.spineCap = newCap
+            			// We can't immediately free the old spine
+            			// since a concurrent push with a lower index
+            			// could still be reading from it. We let it
+            			// leak because even a 1TB heap would waste
+            			// less than 2MB of memory on old spines. If
+            			// this is a problem, we could free old spines
+            			// during STW.
+            		}
+
+            		// Allocate a new block and add it to the spine.
+            		// block = (*gcSweepBlock)(persistentalloc(unsafe.Sizeof(gcSweepBlock{}), sys.CacheLineSize, &memstats.gc_sys))
+            		// blockp := add(b.spine, sys.PtrSize*top)
+            		// // Blocks are allocated off-heap, so no write barrier.
+            		// atomic.StorepNoWB(blockp, unsafe.Pointer(block))
+            		// atomic.Storeuintptr(&b.spineLen, spineLen+1)
+            		// unlock(&b.spineLock)
+                }
+            }
+        };
+        //
+        // 	// We have a block. Insert the span.
+        // 	block.spans[bottom] = s
+        block.spans[bottom] = span;
+    }
+}
